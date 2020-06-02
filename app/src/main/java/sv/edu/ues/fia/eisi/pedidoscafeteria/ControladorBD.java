b4f46b1da72ae25672a446e88d40606d4905e6db
Nicknamempty <fermerinonew@gmail.com>
2020-06-02 17:21:27 -0600

    Solo creé la clase controlador para poder repartirnos los cruds y las partes

---
+++ app/src/main/java/sv/edu/ues/fia/eisi/pedidoscafeteria/ControladorBD.java
@@ -0,0 +1,30 @@
+package sv.edu.ues.fia.eisi.pedidoscafeteria;
+
+public class ControladorBD {
+
+    //aquí va la generación de la base de datos
+
+
+
+
+
+    //aquí termina la generación de la base de datos
+
+
+    //Preguntar a Spiderman si esta parte del código falla o hay dudas
+
+
+
+
+
+
+    //Aquí termina la ayuda de tu vecino el hombre araña
+
+    //Preguntar a Vane si esta parte del código falla o hay dudas
+
+    //Aquí termina la parte de Vane
+
+
+    //Integridad
+    //fin integridad
+}

557392bf6436e5c781244b6f0de17b7b55020946
Nicknamempty <fermerinonew@gmail.com>
2020-06-02 17:59:28 -0600

    este Vane xd

--- app/src/main/java/sv/edu/ues/fia/eisi/pedidoscafeteria/ControladorBD.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/pedidoscafeteria/ControladorBD.java
@@ -1,18 +1,59 @@
 package sv.edu.ues.fia.eisi.pedidoscafeteria;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
-public class ControladorBD {
+import androidx.annotation.Nullable;
 
-    //aquí va la generación de la base de datos
+public class ControladorBD extends SQLiteOpenHelper {
+
+    private static final String nombreBD = "cafeteriaUES.s3db";
 
 
+    public ControladorBD(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
 
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("");
+        db.execSQL("");
+        db.execSQL("");
+        db.execSQL("");
+        db.execSQL("");
+        db.execSQL("");
+        db.execSQL("");
+        db.execSQL("");
+        db.execSQL("");
+        db.execSQL("");
+        db.execSQL("");
+        db.execSQL("");
+        db.execSQL("");
+        db.execSQL("");
 
+    }
 
-    //aquí termina la generación de la base de datos
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
 
 
     //Preguntar a Spiderman si esta parte del código falla o hay dudas
 
+    //CRUD usuario
+    public String insertar(Usuario usuario){
+
+        String resultado="Usuario creado, bienvenido "+usuario;
+        long  comprobador = 0;
+        ContentValues usu = new ContentValues();
+
+
+
+    return resultado;
+    }
 
 
 

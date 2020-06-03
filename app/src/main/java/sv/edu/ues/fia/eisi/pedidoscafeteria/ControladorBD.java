f9e85426d748ca206838c0bd2a92bdaf734730a3
Nicknamempty <fermerinonew@gmail.com>
2020-06-02 21:06:07 -0600

    el database

--- app/src/main/java/sv/edu/ues/fia/eisi/pedidoscafeteria/ControladorBD.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/pedidoscafeteria/ControladorBD.java
@@ -1,6 +1,7 @@
 package sv.edu.ues.fia.eisi.pedidoscafeteria;
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -9,6 +10,13 @@ import androidx.annotation.Nullable;
 public class ControladorBD extends SQLiteOpenHelper {
 
     private static final String nombreBD = "cafeteriaUES.s3db";
+    private SQLiteDatabase db;
+    //campos de las bases
+    private static final String[] camposTabla = {"","","","","",""};
+    private static final String[] camposUsuario = {"","","","","",""};
+    private static final String[] camposDetallePedido = {"","","","","",""};
+    private static final String[] camposMenu = {"","","","","",""};
+    private static final String[] camposProducto = {"","","","","",""};
 
 
     public ControladorBD(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
@@ -44,16 +52,60 @@ public class ControladorBD extends SQLiteOpenHelper {
     //Preguntar a Spiderman si esta parte del código falla o hay dudas
 
     //CRUD usuario
-    public String insertar(Usuario usuario){
+    public String insertar(Usuario usuario) {
 
-        String resultado="Usuario creado, bienvenido "+usuario;
-        long  comprobador = 0;
+        String resultado = "Usuario creado, bienvenido " + usuario;
+        long comprobador = 0;
         ContentValues usu = new ContentValues();
+        usu.put("idUsuario", usuario.getIdUsuario());
+        usu.put("idTipoUsuario", usuario.getIdTipoUsuario());
+        usu.put("nombreUsuario", usuario.getNombreUsuario());
+        usu.put("apellidoUsuario", usuario.getApellidoUsuario());
+        usu.put("teleUsuario", usuario.getTeleUsuario());
+        usu.put("contrasena", usuario.getContrasena());
 
 
+        //comprobando integridad
+        String[] tipoU = {String.valueOf(usuario.getIdTipoUsuario())};
+        Cursor cur = db.query("Usuario",null,"idTipoUsuario = ?",tipoU,null,null,null);
 
-    return resultado;
+        if(cur.moveToFirst())
+        {
+            resultado="El tipo de usuario no existe";
+        }
+        else{
+            comprobador = db.insert("usuario", null, usu);
+
+            if (comprobador == -1 || comprobador == 0)
+            {
+                resultado="Oh, oh, parece que ese id de usuario ya existe, prueba con otro";
+
+            }
+
+        }
+
+            return resultado;
     }
+    public Usuario ConsultaUsuario(String idUsuario)
+    {
+        String[] id = {idUsuario};
+        Cursor cur = db.rawQuery("select * from Usuario where idUsuario ="+idUsuario,null);
+        if(cur.moveToFirst())
+        {
+            Usuario usu1 = new Usuario();
+            usu1.setIdTipoUsuario(Integer.parseInt(cur.getString(0)));
+
+
+        }
+
+
+
+
+
+        return null;
+    }
+
+
 
 
 

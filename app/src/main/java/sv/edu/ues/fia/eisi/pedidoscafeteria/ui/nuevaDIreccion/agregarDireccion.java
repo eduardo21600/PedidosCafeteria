297c61267d3456de1e7728129db503082ffaf99a
Revenge <phoenixwright1998@gmail.com>
2020-06-15 23:42:29 -0600

    insertar ubicacion con WS

--- app/src/main/java/sv/edu/ues/fia/eisi/pedidoscafeteria/ui/nuevaDIreccion/agregarDireccion.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/pedidoscafeteria/ui/nuevaDIreccion/agregarDireccion.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.EditText;
 
 
+import com.shashank.sony.fancytoastlib.FancyToast;
+
 import sv.edu.ues.fia.eisi.pedidoscafeteria.ControladorServicios;
 import sv.edu.ues.fia.eisi.pedidoscafeteria.R;
 import sv.edu.ues.fia.eisi.pedidoscafeteria.Ubicacion;
@@ -20,7 +22,8 @@ public class agregarDireccion extends AppCompatActivity {
     Button obtenerDireccion, agregarDireccion;
     EditText nombreUbicacion, dirUbicacion, facUbicacion, puntoUbicacion;
     ControladorServicios controladorServicios;
-    boolean ACTUALIZAR = true, INSERTAR = false;
+    boolean ACTUALIZAR = false, INSERTAR = true;
+    String resultado;
 
 
     @Override
@@ -31,6 +34,7 @@ public class agregarDireccion extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
+        controladorServicios = new ControladorServicios();
         obtenerDireccion = (Button) findViewById(R.id.obtener_direccion);
         agregarDireccion = (Button) findViewById(R.id.agregar_direccion);
         nombreUbicacion = (EditText) findViewById(R.id.et_nombre_ubicacion);
@@ -49,9 +53,17 @@ public class agregarDireccion extends AppCompatActivity {
         agregarDireccion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Ubicacion ubicacion = new Ubicacion(1,
-                        facUbicacion.getText().toString(), 1, dirUbicacion.getText().toString(), nombreUbicacion.getText().toString(), puntoUbicacion.getText().toString());
-                controladorServicios.CrearAct(ubicacion, getApplicationContext(), INSERTAR);
+                Ubicacion ubicacion = new Ubicacion(1, facUbicacion.getText().toString(), 1, dirUbicacion.getText().toString(), nombreUbicacion.getText().toString(), puntoUbicacion.getText().toString());
+                resultado = controladorServicios.CrearAct(ubicacion, getApplicationContext(), INSERTAR);
+
+                if(resultado == "CONEXIÓN EXITOSA")
+                {
+                    FancyToast.makeText(getApplicationContext(), "Se ha guardado su ubicacion", FancyToast.LENGTH_SHORT, FancyToast.SUCCESS, R.drawable.exito, false).show();
+                }
+                else
+                {
+                    FancyToast.makeText(getApplicationContext(), "No se pudo guardar su ubicacion", FancyToast.LENGTH_SHORT, FancyToast.ERROR, R.drawable.error, false).show();
+                }
             }
         });
 

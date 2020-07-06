b72be894d295629de1aa618d923e6ee2d4ebb761
VanessaAlfaro <vanessaalfarocoreas@gmail.com>
2020-07-05 22:11:18 -0600

    Chatxd

--- app/src/main/java/sv/edu/ues/fia/eisi/pedidoscafeteria/Chat2.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/pedidoscafeteria/Chat2.java
@@ -103,7 +103,7 @@ public class Chat2 extends AppCompatActivity {
             {
                 Toast.makeText(this, "No tendrías que estar viendo esto ¿no creaste un pedidorealizado?", Toast.LENGTH_SHORT).show();
             }
-            
+
         }
 
         //
